e0bb2aaacfb3b3f19d2626c0269abba69310091c
Vyasdev <65308856+Vyasdev217@users.noreply.github.com>
2022-05-07 19:13:31 +0530

    Add files via upload

---
+++ sorting/InsertionSort.c
@@ -0,0 +1,38 @@
+//Bubble sort
+#include<stdio.h>
+
+void swap(int *a,int *b){
+	int temp=*b;
+	*b=*a;
+	*a=temp;
+}
+
+void printarr(int *arr,int size){
+	for(int i=0;i<size;i++)
+		printf("%d ",*(arr+i));
+}
+
+void InsertionSort(int *arr,int size){
+	for(int i=0;i<size;i++){
+		for(int j=i;j>0;j--){
+			if(*(arr+j)>=*(arr+j-1)) break;
+            swap(arr+j,arr+j-1);
+		}
+	}
+}
+
+int main(){
+    // input array++
+
+    int arr[]={1,8,5,2,31,17};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    printf("Insertion sort\n");
+    printf("INPUT  : ");
+    printarr(arr,size);
+    printf("\n");
+    InsertionSort(arr,size);
+    printf("OUTPUT : ");
+    printarr(arr,size);
+    printf("\n");
+    return 0;
+}
